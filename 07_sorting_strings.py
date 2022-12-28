841c02eefc9d0a43a30e17cee397508dc8e67d1b
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2022-12-28 10:14:00 +0530

    Add files via upload

---
+++ 07_sorting_strings.py
@@ -0,0 +1,16 @@
+# sor=[]
+# n=int(input("How many strings?"))
+# for i in range(n):
+#     s=input("Enter String:")
+#     sor.append(s)
+# sor.sort(reverse=True)
+# print(sor)
+# print(sor[::-1])
+# print(*sor)
+# for i in sor:
+#     print(i)
+
+
+# a=['hi','Hey','python','Program']
+# a.sort()
+# print(a)
